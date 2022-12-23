ad50a49543b1e5ccf313bed8e8f173923643ef2f
yan <yan_1731@hotmail.com>
2022-12-23 08:40:28 -0500

    Add ex02

---
+++ cpp00/ex02/src/Account.cpp
@@ -0,0 +1,51 @@
+#include "../include/Account.hpp"
+
+class Account {
+
+
+public:
+
+	typedef Account		t;
+
+	static int	getNbAccounts( void );
+	static int	getTotalAmount( void );
+	static int	getNbDeposits( void );
+	static int	getNbWithdrawals( void );
+	static void	displayAccountsInfos( void );
+
+	Account( int initial_deposit );
+	~Account( void );
+
+	void	makeDeposit( int deposit );
+	bool	makeWithdrawal( int withdrawal );
+	int		checkAmount( void ) const;
+	void	displayStatus( void ) const;
+
+
+private:
+
+	static int	_nbAccounts;
+	static int	_totalAmount;
+	static int	_totalNbDeposits;
+	static int	_totalNbWithdrawals;
+
+	static void	_displayTimestamp( void );
+
+	int				_accountIndex;
+	int				_amount;
+	int				_nbDeposits;
+	int				_nbWithdrawals;
+
+	Account( void );
+
+};
+
+Account::Account(int initial_deposit): _amount(initial_deposit), _nbDeposits(1), _nbWithdrawals(0), _accountIndex(0)
+{
+	return ;
+}
+
+Account::~Account()
+{
+	return ;
+}
