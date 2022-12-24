798ba450891025d41679bd773c633914ac7fc4d1
fenix144 <yan_1731@hotmail.com>
2022-12-24 12:03:08 -0500

    replace is much easier to use

---
+++ cpp01/ex04/Replace.hpp
@@ -0,0 +1,17 @@
+#ifndef REPLACE_HPP
+#define REPLACE_HPP
+#include <iostream>
+
+class Replace
+{
+private:
+	std::string _fileName;
+	std::string _s1;
+	std::string _s2;
+public:
+	Replace();
+	~Replace();
+
+};
+
+#endif
---
+++ cpp01/ex04/main.cpp
@@ -0,0 +1,12 @@
+#include "Replace.hpp"
+
+int main(int argc, char *argv[])
+{
+	if (argc > 4)
+		std::cout << "Too many arguments" << std::endl;
+	else
+	{
+		
+	}
+	return (0);
+}
