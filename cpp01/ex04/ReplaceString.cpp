0830b4802fbd1067458fcb3f0fe8b775d420afd5
fenix144 <yan_1731@hotmail.com>
2022-12-27 13:40:50 -0500

    Starting to figure this out

--- cpp01/ex04/ReplaceString.cpp
+++ cpp01/ex04/ReplaceString.cpp
@@ -15,10 +15,26 @@ void ReplaceString::replace(void)
 {
 	_newFileName.append(".replace");
 	std::string bufferString;
+	std::string newString;
 	std::ifstream input(_fileName);
 	std::ofstream stream(_newFileName);
 	while (std::getline(input, bufferString, '\n'))
 	{
-		stream << bufferString;
+		if (bufferString.find(_s1) != std::string::npos)
+		{
+			while (bufferString.find(_s1, 0) != std::string::npos)
+			{
+				size_t pos = bufferString.find(_s1);
+				newString.append(bufferString.substr(0, pos));
+				bufferString = bufferString.substr(pos + _s1.length(), std::string::npos);
+				newString.append(_s2);
+			}
+			newString.append(bufferString);
+			stream << newString << std::endl;
+		}
+		else
+			stream << bufferString << std::endl;
 	}
+	input.close();
+	stream.close();
 }
