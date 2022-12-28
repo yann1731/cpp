d2969c99f8b2bcaa6ca07bb3023d5fd71f8269fb
fenix144 <yan_1731@hotmail.com>
2022-12-28 16:25:05 -0500

    anddddd open

--- cpp03/ex00/ClapTrap.hpp
+++ cpp03/ex00/ClapTrap.hpp
@@ -5,9 +5,20 @@
 class ClapTrap
 {
 private:
-
+	std::string _name;
+	int _hitPoints;
+	int _energyPoints;
+	int _attackDamage;
 public:
-
+	ClapTrap();
+	ClapTrap(std::string name);
+	ClapTrap(const ClapTrap &claptrap);
+	~ClapTrap();
+	void attack(const std::string &target);
+	void takeDamage(unsigned int amount);
+	void beRepaired(unsigned int amount);
+	std::string getName(void);
+	ClapTrap &operator= (const ClapTrap &claptrap);
 };
 
 #endif
---
+++ cpp03/ex00/main.cpp
@@ -0,0 +1,11 @@
+#include "ClapTrap.hpp"
+
+int main(void)
+{
+	ClapTrap myPal("Claptrap");
+	ClapTrap badClap("Bad claptrap");
+	ClapTrap marvin;
+
+	myPal.attack(badClap.)	
+	return (0);
+}
