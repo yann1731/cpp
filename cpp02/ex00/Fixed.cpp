03dc676e864677be566bb4a7cf484528b362f038
yannick st-laurent <yst-laur@student.42quebec.com>
2022-12-27 19:19:59 -0500

    Let's go with orthodox canonical classes

---
+++ cpp02/ex00/Fixed.cpp
@@ -0,0 +1,44 @@
+#include "Fixed.hpp"
+
+// class Fixed
+// {
+// private:
+//     int _value;
+//     static const int _fractional_bits = 8;
+// public:
+//     Fixed();
+//     Fixed(const Fixed &fixed);
+//     Fixed &operator=(const Fixed &fixed);
+//     ~Fixed();
+//     int getRawBits(void) const;
+//     void setRawBits(int const raw);
+// };
+
+Fixed::Fixed(): _value(0)
+{
+}
+
+Fixed::Fixed(const Fixed &fixed)
+{
+    _value = fixed._value;
+}
+
+Fixed &Fixed::operator= (const Fixed &fixed)
+{
+    Fixed newFixed(fixed);
+    return (newFixed);
+}
+
+Fixed::~Fixed ()
+{
+}
+
+int Fixed::getRawBits(void) const
+{
+    return (_value);
+}
+
+void Fixed::setRawBits(int const raw)
+{
+    _value = raw;
+}
