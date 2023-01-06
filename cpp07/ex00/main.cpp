3ac9b8418542023729cd3b07cf81b7536e508766
yan <yan_1731@hotmail.com>
2023-01-05 22:02:23 -0500

    let's go

--- cpp07/ex00/main.cpp
+++ cpp07/ex00/main.cpp
@@ -1,11 +1,17 @@
 #include "whatever.hpp"
 
-int main(void)
-{
-	int a = 6;
-	int b = 10;
-	int c = 10;
-
-	std::cout << min<int>(b, a) << std::endl;
-	std::cout << max<int>(b, a) << std::endl;
+int main( void ) {
+	int a = 2;
+	int b = 3;
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	return 0;
 }
