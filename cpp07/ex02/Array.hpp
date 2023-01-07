a3685f06705e9d4610999012d509561854cd5f52
yan <yan_1731@hotmail.com>
2023-01-07 11:33:43 -0500

    let's go

--- cpp07/ex02/Array.hpp
+++ cpp07/ex02/Array.hpp
@@ -1,41 +1,34 @@
 #include <exception>
-// Develop a class template Array that contains elements of type T and that implements
-// the following behavior and functions:
-// •Construction with no parameter: Creates an empty array.
-// •Construction with an unsigned int n as a parameter: Creates an array of n elements
-// initialized by default.
-// Tip: Try to compile int * a = new int(); then display *a.
-// •Construction by copy and assignment operator. In both cases, modifying either the
-// original array or its copy after copying musn’t affect the other array.
-// •You MUST use the operator new[] to allocate memory. Preventive allocation (al-
-// locating memory in advance) is forbidden. Your program must never access non-
-// allocated memory.
-// •Elements can be accessed through the subscript operator: [ ].
-// •When accessing an element with the [ ] operator, if its index is out of bounds, an
-// std::exception is thrown.
-// •A member function size() that returns the number of elements in the array. This
-// member function takes no parameter and musn’t modify the current instance.
-// As usual, ensure everything works as expected and turn in a main.cpp file that con-
-// tains your tests.
+#include <iostream>
 
 template <typename T>
 class Array
 {
 private:
-	T *_e
+	T *_e;
 	size_t _size;
 public:
-	Array(): _size(0), _e(new T[0]) {};
-	Array(size_t n): _size(n), _e(new T[n]) {};
-	Array(const Array &rhs): _size(rhs._size), _e(new T[rhs._size])
+	Array(): _e(new T[0]), _size(0) {};
+	Array(size_t n): _e(new T[n]), _size(n) {};
+	Array(const Array &rhs): _e(new T[rhs._size]), _size(rhs._size)
 	{
-		for (size_t i = 0; i < rhs.size; i++)
+		for (size_t i = 0; i < rhs._size; i++)
 			_e[i] = rhs._e[i];
+	};
+	~Array()
+	{
+		delete [] this->_e;
 	};
 	Array &operator=(const Array &rhs)
 	{
-		for (size_t i = 0; i < rhs.size; i++)
-			_e[i] = rhs._e[i];
+		if (this != rhs)
+		{
+			_size = rhs._size;
+			delete [] this->_e;
+			this->_e = new T[rhs._size];
+			for (size_t i = 0; i < rhs._size; i++)
+				_e[i] = rhs._e[i];
+		}
 		return *this;
 	};
 	T &operator[](unsigned int index)
@@ -55,7 +48,7 @@ public:
 	{
 		const char *what() const throw()
 		{
-			return "Index used is out of bounds";
+			return "Exception: Index is out of bounds";
 		}
 	};
 };
---
+++ cpp07/ex02/main.cpp
@@ -0,0 +1,44 @@
+#include "Array.hpp"
+
+int main(void)
+{
+    Array<int> array(10);
+    Array<char> string(5);
+
+    string[0] = 'H';
+    string[1] = 'e';
+    string[2] = 'l';
+    string[3] = 'l';
+    string[4] = 'o';
+
+    Array<char> strCopy = string;
+
+    array[1] = 2;
+    array[0] = 3;
+
+    std::cout << array.size() << std::endl;
+    std::cout << array[1] << " " << array[0] << std::endl;
+    for (int i = 0; i < 5; i++)
+    {
+        std::cout << string[i];
+        if (i == 4)
+            std::cout << std::endl;
+    }
+    for (int i = 0; i < 5; i++)
+    {
+        std::cout << strCopy[i];
+        if (i == 4)
+            std::cout << std::endl;
+    }
+
+    try
+    {
+        array[11];
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    
+    return 0;
+};
