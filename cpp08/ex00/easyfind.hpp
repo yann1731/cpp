7b9d8ae49bbe45975237cc536a8367e1233ca70b
yan <yan_1731@hotmail.com>
2023-01-08 21:41:18 -0500

    let's go

---
+++ cpp08/ex00/easyfind.hpp
@@ -0,0 +1,12 @@
+#ifndef EASYFIND_HPP
+#define EASYFIND_HPP
+#include <algorithm>
+#include <vector>
+
+template <typename T>
+void easyfind(T container, int toFind)
+{
+    container( )
+}
+
+#endif
