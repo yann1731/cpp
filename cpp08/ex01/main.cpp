8f6fcea39e2b8c8adadc40e02002b17260f6a1a0
yan <yan_1731@hotmail.com>
2023-01-09 13:05:48 -0500

    let's go

--- cpp08/ex01/main.cpp
+++ cpp08/ex01/main.cpp
@@ -1,29 +1,36 @@
 #include "Span.hpp"
 
-int main()
+int main(void)
 {
-    Span sp = Span(5);
-    sp.addNumber(6);
-    sp.addNumber(3);
-    sp.addNumber(17);
-    sp.addNumber(9);
-    sp.addNumber(11);
-    std::cout << sp.shortestSpan() << std::endl;
-    std::cout << sp.longestSpan() << std::endl;
-    return 0; 
+    try
+    {
+        Span sp = Span(5);
+        sp.addNumber(6);
+        sp.addNumber(3);
+        sp.addNumber(17);
+        sp.addNumber(9);
+        sp.addNumber(11);
+        std::cout << sp.shortestSpan() << std::endl;
+        std::cout << sp.longestSpan() << std::endl;
+        Span newSpan(10);
+        Span shitSpan(2);
+
+        shitSpan.addNumber(2);
+        shitSpan.addNumber(4);
+    
+        srand(time(NULL));
+
+        for (int i = 0; i < 10; i++)
+        {
+            newSpan.addNumber(rand() % 100);
+        }
+        std::cout << newSpan.longestSpan() << std::endl;
+        std::cout << newSpan.shortestSpan() << std::endl;
+        std::cout << shitSpan.longestSpan() << std::endl;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    return 0;
 }
-
-// int main(void)
-// {
-//     Span newSpan(10);
-
-//     srand(time(NULL));
-
-//     for (int i = 0; i < 10; i++)
-//     {
-//         newSpan.addNumber(rand() % 100);
-//     }
-//     std::cout << newSpan.longestSpan() << std::endl;
-//     std::cout << newSpan.shortestSpan() << std::endl;
-//     return 0;
-// }
