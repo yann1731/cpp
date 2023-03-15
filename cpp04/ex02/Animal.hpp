772f554a2eb659bd30672603bde88cacd04acd32
fenix144 <yan_1731@hotmail.com>
2023-03-15 10:46:25 -0400

    Turned Animal into abstract class

--- cpp04/ex02/Animal.hpp
+++ cpp04/ex02/Animal.hpp
@@ -10,8 +10,8 @@ public:
     Animal();
     Animal(const std::string &type);
     Animal(const Animal &animal);
-    virtual ~Animal();
-    virtual void makeSound(void) const;
+    virtual ~Animal() = 0;
+    virtual void makeSound(void) const = 0;
     std::string getType(void) const;
     Animal &operator=(const Animal &src);
 };
