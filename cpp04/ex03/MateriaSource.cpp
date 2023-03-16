022cc3f9e858f4592f43815bddf0da0df36a97cc
Yannick St-laurent <yst-laur@c1r8p4.42quebec.com>
2023-03-16 14:19:56 -0400

    Add MateriaSource

--- cpp04/ex03/MateriaSource.cpp
+++ cpp04/ex03/MateriaSource.cpp
@@ -1,2 +1,13 @@
 #include "MateriaSource.hpp"
 
+MateriaSource::MateriaSource() { }
+
+MateriaSource::~MateriaSource() { }
+
+void MateriaSource::learnMateria(AMateria* mat) {
+
+}
+
+AMateria* MateriaSource::createMateria(std::string const & type) {
+	
+}
--- cpp04/ex03/MateriaSource.hpp
+++ cpp04/ex03/MateriaSource.hpp
@@ -5,14 +5,12 @@
 class MateriaSource: public IMateriaSource
 {
 	// virtual ~IMateriaSource() {}
-	// virtual void learnMateria(AMateria*) = 0;
-	// virtual AMateria* createMateria(std::string const & type) = 0;
 private:
 	AMateria* inventory[4];
 public:
 	MateriaSource();
 	~MateriaSource();
-	void learMateria(AMateria *mat);
+	void learnMateria(AMateria *mat);
 	AMateria *createMateria(std::string const & type);
 };
 
