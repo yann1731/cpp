c2ab62c0c6a09045476ed3c56cb11ca75a6a7978
Yannick St-laurent <yst-laur@c1r8p4.42quebec.com>
2023-03-22 14:27:24 -0400

    Remove new character creation

--- cpp04/ex03/main.cpp
+++ cpp04/ex03/main.cpp
@@ -8,18 +8,13 @@ int main(void)
 	IMateriaSource* src = new MateriaSource();
 	src->learnMateria(new Ice());
 	src->learnMateria(new Cure());
-	std::cout << "Learn materia called" << std::endl;
 	ICharacter* me = new Character("me");
 	AMateria* tmp;
-	std::cout << "AMateria tmp declared" << std::endl;
 	tmp = src->createMateria("ice");
-	std::cout << "CreateMateria called" << std::endl;
 	me->equip(tmp);
-	std::cout << "me equipping tmp" << std::endl;
 	tmp = src->createMateria("cure");
 	me->equip(tmp);
 	ICharacter* bob = new Character("bob");
-	ICharacter* bob2 = new Character;
 	me->use(0, *bob);
 	me->use(1, *bob);
 	delete bob;
