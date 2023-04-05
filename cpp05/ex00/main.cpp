845a04e5e9b1f421e11bbd42e61c38a26ba1a0cd
fenix144 <yan_1731@hotmail.com>
2023-04-05 17:40:02 -0400

    let's go

--- cpp05/ex00/main.cpp
+++ cpp05/ex00/main.cpp
@@ -5,27 +5,46 @@ int main(void)
     try
     {
         Bureaucrat randomBureaucrat;
-        Bureaucrat marvin("Marvin the miserable", 34);
-        Bureaucrat numberOne("Number one", 1);
-        Bureaucrat b = marvin;
-        std::cout << b << std::endl;
-        b.promote();
-        std::cout << b.getGrade() << std::endl;
-        std::cout << marvin.getGrade() << std::endl;
-        std::cout << marvin << std::endl;
-        std::cout << randomBureaucrat << std::endl;
-        std::cout << numberOne << std::endl;
-        //numberOne.promote();
+        randomBureaucrat.promote();
         randomBureaucrat.promote();
         std::cout << randomBureaucrat << std::endl;
-        randomBureaucrat.demote();
-        randomBureaucrat.demote();
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+
+    try
+    {
+        Bureaucrat randomGoon;
+        randomGoon.demote();
     }
     catch(const std::exception& e)
     {
         std::cerr << e.what() << '\n';
     }
     
+    try
+    {
+        Bureaucrat numberOne("Number one", 1);
+        numberOne.promote();
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+
+    try
+    {
+        Bureaucrat numberOne("Number one", 1);
+        numberOne.demote();
+        numberOne.demote();
+        std::cout << numberOne << std::endl;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
 
     return 0;
 }
