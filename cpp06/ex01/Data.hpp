eb2a9c0028c1731623385d3567ea005e63e5583c
fenix144 <yan_1731@hotmail.com>
2023-04-24 18:55:42 -0400

    Add more functions for testing of Data struct

---
+++ cpp06/ex01/Data.hpp
@@ -0,0 +1,16 @@
+#ifndef DATA_HPP
+#define DATA_HPP
+
+struct Data
+{
+    Data();
+    ~Data();
+	double posx;
+	double posy;
+	double dirx;
+	double diry;
+	int hp;
+	int mp;
+};
+
+#endif
--- cpp06/ex01/function.hpp
+++ cpp06/ex01/function.hpp
@@ -1,16 +1,7 @@
 #ifndef FUNCTION_HPP
 #define FUNCTION_HPP
-# include <iostream>
-
-struct Data
-{
-	double posx;
-	double posy;
-	double dirx;
-	double diry;
-	int hp;
-	int mp;
-};
+# include <Data.hpp>
+# include <cstdint>
 
 uintptr_t serialize(Data* ptr);
 Data* deserialize(uintptr_t raw);
