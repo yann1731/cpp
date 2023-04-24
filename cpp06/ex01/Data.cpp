6ccab5e169691b3caed465319a98728d1281eac6
fenix144 <yan_1731@hotmail.com>
2023-04-24 19:02:22 -0400

    Add tests

--- cpp06/ex01/Data.cpp
+++ cpp06/ex01/Data.cpp
@@ -1,12 +1,5 @@
 #include "Data.hpp"
 
-	// double posx;
-	// double posy;
-	// double dirx;
-	// double diry;
-	// int hp;
-	// int mp;
-
 Data::Data():posx(12.5), posy(20.5), dirx(1), diry(0), hp(100), mp(0) {
 
 }
--- cpp06/ex01/main.cpp
+++ cpp06/ex01/main.cpp
@@ -5,15 +5,17 @@ int main(void)
 	Data *ptr = new Data;
 	uintptr_t deser = 0;
 
-	std::cout << ptr << std::endl;
+	std::cout << *ptr << std::endl;
 
 	deser = serialize(ptr);
+
+	ptr = NULL;
 
 	std::cout << deser << std::endl;
 
 	ptr = deserialize(deser);
 
-	std::cout << ptr << std::endl;
+	std::cout << *ptr << std::endl;
 
 	delete ptr;
 }
