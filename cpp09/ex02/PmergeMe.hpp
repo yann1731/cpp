db62820ab6a9d3ad4d595ad9445aefe372cfaf6f
fenix144 <yan_1731@hotmail.com>
2023-04-28 14:20:01 -0400

    Add class declaration for PmergeMe

--- cpp09/ex02/PmergeMe.hpp
+++ cpp09/ex02/PmergeMe.hpp
@@ -1,3 +1,22 @@
 #pragma once
 # include <iostream>
+# include <exception>
 
+
+using std::cerr;
+using std::cout;
+using std::endl;
+using std::exception;
+
+class PmergeMe {
+private:
+    PmergeMe();
+    PmergeMe(const PmergeMe& other);
+    PmergeMe& operator=(const PmergeMe& rhs);
+public:
+    PmergeMe(char **argv);
+    ~PmergeMe();
+    class InvalidNumber: public exception {
+        const char* what() const throw();
+    };
+};
--- cpp09/ex02/main.cpp
+++ cpp09/ex02/main.cpp
@@ -1,7 +1,13 @@
 #include "PmergeMe.hpp"
 
 int main(int argc, char **argv) {
-    
+    if (argc == 2) {
+        PmergeMe pMerge(argv);
+    }
+    else {
+        std::cerr << "Program needs 1 argument only" << std::endl;
+        return 1;
+    }
     
     return 0;
 }
