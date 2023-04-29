e7b0c17b609575ecb42d31aaa541b04ce83cd88d
fenix144 <yan_1731@hotmail.com>
2023-04-29 19:23:50 -0400

    Add readToBuffer function

--- cpp09/ex02/PmergeMe.cpp
+++ cpp09/ex02/PmergeMe.cpp
@@ -1,2 +1,17 @@
 #include "PmergeMe.hpp"
 
+PmergeMe::PmergeMe(char **argv) {
+    try
+    {
+        readToBuffer(argv);
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+        exit(1);
+    }
+}
+
+void PmergeMe::readToBuffer(char **argv) {
+    _buffer = argv[1];
+}
--- cpp09/ex02/PmergeMe.hpp
+++ cpp09/ex02/PmergeMe.hpp
@@ -1,22 +1,30 @@
 #pragma once
 # include <iostream>
 # include <exception>
+# include <fstream>
 
 
 using std::cerr;
 using std::cout;
 using std::endl;
 using std::exception;
+using std::string;
 
 class PmergeMe {
 private:
     PmergeMe();
     PmergeMe(const PmergeMe& other);
     PmergeMe& operator=(const PmergeMe& rhs);
+    string _buffer;
+    void readToBuffer(char **argv);
 public:
     PmergeMe(char **argv);
+
     ~PmergeMe();
     class InvalidNumber: public exception {
+        const char* what() const throw();
+    };
+    class OpenFail: public exception {
         const char* what() const throw();
     };
 };
