98201f65f86c31072674f7c2cc52f2c126580781
fenix144 <yan_1731@hotmail.com>
2023-05-08 11:07:03 -0400

    Add try/catch to iter

--- cpp07/ex01/iter.hpp
+++ cpp07/ex01/iter.hpp
@@ -5,8 +5,9 @@
 template <typename A>
 void iter(A *array, size_t len, void (*f)(A&))
 {
-    for (size_t i = 0; i < len; i++)
-        (*f)(array[i]);
+    for (size_t i = 0; i < len; i++) {
+		(*f)(array[i]);
+	}
 }
 
 #endif
--- cpp07/ex01/main.cpp
+++ cpp07/ex01/main.cpp
@@ -2,7 +2,7 @@
 
 void addOne(int &num)
 {
-    num++;
+	num++;
 }
 
 void printString(std::string &str)
@@ -12,20 +12,26 @@ void printString(std::string &str)
 
 int main(void)
 {
-    int a[12];
-    std::string str[5] = {"Hello ", "Comment ", "Ca ", "Vas ", "Okbyela"};
+	try {
+		int a[12];
+		std::string str[5] = {"Hello ", "Comment ", "Ca ", "Vas ", "Okbyela"};
 
-    for (int i = 0; i < 12; i++)
-        a[i] = i + 1;
-    for (int i = 0; i < 12; i++)
-        std::cout << a[i] << std::endl;
+		for (int i = 0; i < 12; i++)
+			a[i] = i + 1;
+		for (int i = 0; i < 12; i++)
+			std::cout << a[i] << std::endl;
 
-    iter(a, 12, &addOne);
+		iter(a, 12, &addOne);
 
-    for (int i = 0; i < 12; i++)
-        std::cout << a[i] << std::endl;
+		for (int i = 0; i < 12; i++)
+			std::cout << a[i] << std::endl;
 
-    iter(str, 5, &printString);
+		iter(str, 5, &printString);
+	}
+	catch (const std::exception& e) {
+		cerr << e.what() << endl;
+	}
+
 
     return 0;
 }
